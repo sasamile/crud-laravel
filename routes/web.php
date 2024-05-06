f8ab018477ecdf4258938cf2d211c93d5e743e33
Santiago Suescun <116961553+sasamile@users.noreply.github.com>
2024-05-06 12:16:26 -0500

    Tabla task

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('tasks',TaskController::class);
