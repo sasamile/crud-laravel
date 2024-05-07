6dbef57ddda903361dcd871a55014edc7cfd8c51
Santiago Suescun <116961553+sasamile@users.noreply.github.com>
2024-05-06 22:27:36 -0500

    Created Metadatos

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <meta name="Nombre" content="Santiago Suescun">
+    <meta name="Docentes" content="Mario Augustos Porras">
+    <meta name="Materia" content="Framewors">
+    <meta name="Fecha" content="22/05/2024">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <title>CRUD laravel 10</title>
